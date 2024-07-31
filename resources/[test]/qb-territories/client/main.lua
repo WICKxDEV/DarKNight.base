98d93efbfda4e2c1baf2dcc9becd34c56a1aa95a
WICKxDEV <isuruwickramasinghe.sliate@gmail.com>
2024-07-31 11:51:06 +0530

    update

---
+++ resources/[test]/qb-territories/client/main.lua
@@ -0,0 +1,135 @@
+-- Define necessary variables and initialize QBCore object
+local Territories = {}
+local insidePoint = false
+local activeZone = nil
+
+local QBCore = exports['qb-core']:GetCoreObject()
+
+isLoggedIn = false
+PlayerGang = {}
+PlayerJob = {}
+
+-- Event handlers for player loading, unloading, gang update, and job update
+RegisterNetEvent('QBCore:Client:OnPlayerLoaded')
+AddEventHandler('QBCore:Client:OnPlayerLoaded', function()
+    isLoggedIn = true
+    PlayerGang = QBCore.Functions.GetPlayerData().gang
+    PlayerJob = QBCore.Functions.GetPlayerData().job
+end)
+
+RegisterNetEvent('QBCore:Client:OnPlayerUnload')
+AddEventHandler('QBCore:Client:OnPlayerUnload', function()
+    isLoggedIn = false
+end)
+
+RegisterNetEvent('QBCore:Client:OnGangUpdate')
+AddEventHandler('QBCore:Client:OnGangUpdate', function(GangInfo)
+    PlayerGang = GangInfo
+    isLoggedIn = true
+end)
+
+RegisterNetEvent('QBCore:Client:OnJobUpdate')
+AddEventHandler('QBCore:Client:OnJobUpdate', function(JobInfo)
+    PlayerJob = JobInfo
+    isLoggedIn = true
+end)
+
+-- Thread to create territory zones and blips on the map
+CreateThread(function()
+    Wait(500)
+    for k, v in pairs(Zones["Territories"]) do
+        local zone = CircleZone:Create(v.centre, v.radius, {
+            name = "greenzone-"..k,
+            debugPoly = Zones["Config"].debug,
+        })
+
+        -- Create blip for the territory radius
+        local blip = AddBlipForRadius(v.centre.x, v.centre.y, v.centre.z, v.radius)
+        SetBlipAlpha(blip, 80) -- Change opacity here
+        SetBlipColour(blip, Zones["Gangs"][v.winner].color or Zones["Gangs"]["neutral"].color)
+
+        -- Create blip for the territory center
+        local blip2 = AddBlipForCoord(v.centre.x, v.centre.y, v.centre.z)
+        SetBlipSprite(blip2, v.blip)
+        SetBlipDisplay(blip2, 4)
+        SetBlipScale(blip2, 0.8)
+        SetBlipAsShortRange(blip2, true)
+        SetBlipColour(blip2, Zones["Gangs"][v.winner].color or Zones["Gangs"]["neutral"].color)
+        BeginTextCommandSetBlipName("STRING")
+        AddTextComponentSubstringPlayerName(Zones["Gangs"][v.winner].name)
+        EndTextCommandSetBlipName(blip2)
+
+        Territories[k] = {
+            zone = zone,
+            id = k,
+            blip = blip
+        }
+    end
+end)
+
+-- Event handler to update territory blips
+RegisterNetEvent("qb-gangs:client:updateblips")
+AddEventHandler("qb-gangs:client:updateblips", function(zone, winner)
+    local colour = Zones["Colours"][winner]
+    local blip = Territories[zone].blip
+    SetBlipColour(blip, colour)
+    BeginTextCommandSetBlipName("STRING")
+    AddTextComponentSubstringPlayerName(winner)
+    EndTextCommandSetBlipName(blip)
+end)
+
+-- Function to check if a territory is contested
+function isContested(tab)
+    local count = 0
+    for _, _ in pairs(tab) do
+        count = count + 1
+    end
+
+    return count > 1 and "contested" or ""
+end
+
+-- Main thread to handle player interaction with territories
+CreateThread(function()
+    while true do 
+        Wait(500)
+        if isLoggedIn then
+            local PlayerPed = PlayerPedId()
+            local pedCoords = GetEntityCoords(PlayerPed)
+            
+            -- Check for contested territories
+            for _, v in pairs(Zones["Territories"]) do
+                while isContested(v.occupants) == "contested" do
+                    -- Handle contested territory logic here
+                end
+            end 
+
+            -- Handle player entering and leaving territories
+            for k, zone in pairs(Territories) do  
+                if Territories[k].zone:isPointInside(pedCoords) then
+                    insidePoint = true
+                    activeZone = Territories[k].id
+                        
+                    TriggerEvent("QBCore:Notify", Lang:t("error.enter_gangzone"), "error")
+          
+                    while insidePoint do   
+                        exports['qb-drawtext']:DrawText(Lang:t("error.hostile_zone"), 'right')
+                        if PlayerGang.name ~= "none" then
+                            TriggerServerEvent("qb-gangs:server:updateterritories", activeZone, true) 
+                        end   
+                        if not Territories[k].zone:isPointInside(GetEntityCoords(PlayerPed)) then
+                            if PlayerGang.name ~= "none" then
+                                TriggerServerEvent("qb-gangs:server:updateterritories", activeZone, false)
+                            end
+                            insidePoint = false
+                            activeZone = nil
+                            QBCore.Functions.Notify(Lang:t("error.leave_gangzone"), "error")
+                        end
+                        Wait(1000)
+                    end
+                    exports['qb-drawtext']:HideText()
+                end
+            end  
+            Wait(2000)
+        end
+    end
+end)
